32698636d4329166c916b1d0d16ce629c7083420
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-15 21:49:33 -0300

    changed language structure

---
+++ src/admin/lang/en_us.js
@@ -0,0 +1,48 @@
+export const _instructions = [
+
+"Welcome to the PamCommerce installation page!",
+"Before proceeding to the next step, make sure your server supports PHP and MySQL and that you have your database hostname, username, and password. In most cases, the hostname is localhost. If you don't have the username and password or don't know how to get them, contact your web host.",
+"Next"	
+	
+];
+
+export const _createDB = [
+	
+"Database name",
+"The default is \"pamcommerce\".",
+"Username",
+"Your database username.",
+"Password",
+"Your database password.",
+"Database host",
+"It's usually localhost, but if it doesn't work, contact your web host.",
+"Submit",
+"We couldn't connect to the database. Check the information provided and try again."	
+	
+];
+
+export const _install = [
+
+"Let's create the username and password to access the PamCommerce dashboard.",
+"Site name",
+"Email",
+"Password",
+"Finish",
+"There was an error in inserting data into the database. Contact the system developer."	
+	
+];
+
+export const _leftSideBar = [
+
+{"Dashboard": "icofont-dashboard-web"},	
+{"Products": "icofont-shopping-cart"},
+{"Categories": "icofont-tags"},
+{"Discounts": "icofont-sale-discount"},
+{"Orders": "icofont-chart-growth"},
+{"Customers": "icofont-users-social"},	
+{"Pages": "icofont-page"},	
+{"Layout": "icofont-layout"},
+{"Settings": "icofont-ui-settings"},	
+{"Admin": "icofont-user"}	
+
+];
---
+++ src/admin/lang/pt_br.js
@@ -0,0 +1,48 @@
+export const _instructions = [
+
+"Bem-vindo à página de instalação do PamCommerce!",
+"Antes de prosseguir para a próxima etapa, certifique-se de que seu servidor tem suporte a PHP e MySQL e que você possui o nome, usuário e senha do host da base de dados. Na grande maioria dos casos, o nome do host é localhost. Caso você não tenha o usuário e a senha ou não saiba como conseguir, entre em contato com o suporte do seu servidor de hospedagem.",
+"Próximo"	
+	
+];
+
+export const _createDB = [
+	
+"Nome da base de dados",
+"O padrão é \"pamcommerce\".",
+"Usuário",
+"Seu usuário da base de dados.",
+"Senha",
+"Sua senha da base de dados.",
+"Host da base de dados",
+"Normalmente o host é localhost, mas caso não funcione, contate seu servidor de hospedagem.",
+"Enviar",
+"Não foi possível conectar a base de dados. Verifique as informações fornecidas e tente novamente."
+	
+];
+
+export const _install = [
+
+"Vamos criar o usuário e senha para acesso ao painel do PamCommerce.",	
+"Nome do site",
+"E-mail",
+"Password",
+"Finalizar",
+"Houve um erro ao inserir dados na base de dados. Contate o desenvolvedor do sistema."	
+	
+];
+
+export const _leftSideBar = [
+
+{"Painel": "icofont-dashboard-web"},	
+{"Produtos": "icofont-shopping-cart"},
+{"Categorias": "icofont-tags"},
+{"Descontos": "icofont-sale-discount"},
+{"Pedidos": "icofont-chart-growth"},
+{"Clientes": "icofont-users-social"},	
+{"Páginas": "icofont-page"},	
+{"Layout": "icofont-layout"},
+{"Configurações": "icofont-ui-settings"},	
+{"Administração": "icofont-user"}	
+
+];
