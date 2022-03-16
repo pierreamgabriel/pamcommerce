d0a188fa7ca1363b9d9cee3e97469c89263be9c7
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-15 22:46:03 -0300

    all styles now imported in style.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
-import Admin from './admin/index'
+import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
