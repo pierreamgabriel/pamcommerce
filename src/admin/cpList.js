b4511d1c1340e05ea9519e65a412e4e7c021798f
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-19 20:53:58 -0300

    list for extracting components name

---
+++ src/admin/cpList.js
@@ -0,0 +1,10 @@
+import { Products, AddProduct } from './components/Products';
+import { Dashboard } from './components/Dashboard';
+
+const cpList = {
+    "Dashboard": Dashboard,
+    "Products": Products,
+    "AddProduct": AddProduct
+  }
+
+export default cpList; 
