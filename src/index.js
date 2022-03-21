2ab6476c8ddfdf7cbc130661c1f0b214a58fbd67
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-21 00:04:57 -0300

    moved to sass

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
-import './style.css';
+import './style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
