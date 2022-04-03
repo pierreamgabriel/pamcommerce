3eacbe794332767e1e69a1c75af71b6a80598714
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-04-03 19:30:31 -0300

    categories component

---
+++ src/admin/components/Categories.js
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+
+export function Categories () {
+    const site = useSelector((state) => state.settings);
+    const langFile = require('../lang/' + site.lang);
+    return(
+        <div className="container">
+        <div className="row">
+          <div className="col">
+          <h5>{langFile.addCategories[0]}</h5>
+          <div className="mb-3">
+            <label className="form-label">{langFile.addCategories[1]}</label>
+            <input className="form-control" type="text" name="name" />
+            <p>{langFile.addCategories[2]}</p>
+            </div>
+            <div className="mb-3">
+            <label className="form-label">{langFile.addCategories[3]}</label>
+            <input className="form-control" type="text" name="urlname" />
+            <p>{langFile.addCategories[4]}</p>
+            </div>
+            <div className="mb-3">
+            <label className="form-label">{langFile.addCategories[5]}</label>
+            <input className="form-control" type="text" name="parent" />
+            <p>{langFile.addCategories[6]}</p>
+            </div>
+            <button type="button" className="btn btn-primary" style={{marginTop: "1rem"}}>{langFile.addCategories[7]}</button>
+          </div>
+          <div className="col">
+            
+          </div>
+        </div>
+      </div>    
+    );
+}
--- src/admin/cpList.js
+++ src/admin/cpList.js
@@ -1,10 +1,12 @@
 import { Products, AddProduct } from './components/Products';
 import { Dashboard } from './components/Dashboard';
+import { Categories } from './components/Categories';
 
 const cpList = {
     "Dashboard": Dashboard,
     "Products": Products,
-    "AddProduct": AddProduct
+    "AddProduct": AddProduct,
+    "Categories": Categories
   }
 
 export default cpList; 
