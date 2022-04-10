7934f549f5609495edbda751456aee8936070583
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-04-09 22:32:52 -0300

    added error true and changed msg 0 to $e

--- src/api/create-db.php
+++ src/api/create-db.php
@@ -6,5 +6,5 @@ $db_create = mysqli_connect($host,$user,$password);
 $sql_create = "CREATE DATABASE IF NOT EXISTS $database";
 $db_create->query($sql_create);	
 } catch(mysqli_sql_exception $e) {
-echo json_encode(["msg" => 0]);	
+echo json_encode(["error" => true, "msg" => $e]);	
 }
